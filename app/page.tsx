65fcedf37dc7f35149ab52c7fe9f3854c2863e03
Mohamed Tarek <m.tarekazab1997@gmail.com>
2024-02-23 20:52:28 +0200

    fix: add test env variable

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <div>Hello NEXT JS</div>
-      {process.env.API_KEY}
+      {process.env.OWNER}
     </main>
   );
 }
