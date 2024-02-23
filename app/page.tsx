71d67cae70dabb340c56a32a855a362a6b2c7889
Mohamed Tarek <m.tarekazab1997@gmail.com>
2024-02-23 20:54:48 +0200

    fix: remove test variable

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <div>Hello NEXT JS</div>
-      {process.env.OWNER}
     </main>
   );
 }
