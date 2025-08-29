999e79687614666637e24a5548c9ea8965cebf35
Mohamed Tarek <m.tarekazab1997@gmail.com>
2025-08-29 13:15:35 +0300

    feat: use template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,137 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import "@/configs/firebase";
-import MainHeader from "./sections/header";
-import MainFooter from "./sections/footer";
-const inter = Inter({ subsets: ["latin"] });
-export const metadata: Metadata = {
-  title: "Mohamed Tarek Azab | Full Stack Developer",
-  description: "Full stack developer",
+
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Inter as FontSans } from "next/font/google";
+import localFont from "next/font/local";
+
+import { Analytics } from "@/components/common/analytics";
+import { ThemeProvider } from "@/components/common/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/providers/modal-provider";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+// Font files can be colocated inside of `pages`
+const fontHeading = localFont({
+  src: "../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+});
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export const metadata = {
+  metadataBase: new URL(siteConfig.url),
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  authors: [
+    {
+      name: siteConfig.authorName,
+      url: siteConfig.url,
+    },
+  ],
+  creator: siteConfig.username,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+    creator: `@${siteConfig.username}`,
+  },
+  icons: {
+    icon: siteConfig.iconIco,
+    shortcut: siteConfig.logoIcon,
+    apple: siteConfig.logoIcon,
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+  alternates: {
+    canonical: siteConfig.url,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  verification: {
+    google: process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION,
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: RootLayoutProps) {
+  const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID;
+  if (!GA_ID) {
+    throw new Error("Missing Google Analytics ID");
+  }
+
   return (
-    <html lang="en" className="h-full box-border">
-      <body className="flex flex-col h-full">
-        <MainHeader />
-        <main className="flex-1 h-auto box-border w-100">
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          fontHeading.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          themes={[
+            "light",
+            "dark",
+            "retro",
+            "cyberpunk",
+            "paper",
+            "aurora",
+            "synthwave",
+          ]}
+        >
           {children}
-        </main>
-        <footer className="bg-slate-200 text-center">
-          <MainFooter />
-        </footer>
+          <Analytics />
+          <Toaster />
+          <ModalProvider />
+        </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId={GA_ID} />
     </html>
   );
 }
--- app/sections/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const footer = () : React.ReactNode => {
-  return (
-    <span>Made with &#9829;, &#169; Copyright {new Date().getFullYear()} reserved for Mohamed Tarek</span>
-  )
-}
-
-export default footer
